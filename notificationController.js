413147e49dc7a9d00f6a14b98ba712ceb6fdb6be
Mrinmoy Chakraborty <themrinmoychakraborty@gmail.com>
2024-03-04 22:23:27 +0530

    Add email notification functionality using Mailtrap

---
+++ notificationController.js
@@ -0,0 +1,26 @@
+const { MailtrapClient } = require("mailtrap");
+
+const TOKEN = "c486150b023a89dbe3d0ba4d260d2baf";
+const ENDPOINT = "https://send.api.mailtrap.io/";
+
+const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
+
+const sender = {
+  email: "mailtrap@mrinmoy.org",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "themrinmoychakraborty@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
