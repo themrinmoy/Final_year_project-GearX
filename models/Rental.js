19eb2d79a372746e7edb4c98a4e199a4b5957e27
Mrinmoy Chakraborty <themrinmoychakraborty@gmail.com>
2024-03-17 23:14:52 +0530

    Add rentaldate field to Rental model

--- models/Rental.js
+++ models/Rental.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const rentalSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     productId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }],
+    rentaldate: { type: Date, default: Date.now },
     rentalStartDate: { type: Date, required: true },
     rentalEndDate: { type: Date, required: true },
     returnDate: { type: Date },
