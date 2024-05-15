3a0ecbcad3135e96d7882b7a6fce922c0ea27761
Mrinmoy Chakraborty <themrinmoychakraborty@gmail.com>
2024-05-15 21:15:53 +0530

    file move for better  structure

--- temp.js
+++
@@ -1,9 +0,0 @@
-// React component logic
-const Dashboard = ({ userType }) => {
-    return (
-        <div>
-            {userType === 'buyer' && <BuyerDashboard />}
-            {userType === 'administrator' && <AdminDashboard />}
-        </div>
-    );
-};
